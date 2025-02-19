2d44fcafa142bf8c50cbcaa707926779f3d198e4
Micaela Rodriguez Steube <micaelarodriguezsteube@Micaelas-MacBook-Pro-2.local>
2025-02-19 11:45:19 -0500

    first version of profile page

--- app/(tabs)/mica.tsx
+++ app/(tabs)/mica.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Button } from 'react-native';
+import { StyleSheet, Button, SafeAreaView, Image } from 'react-native';
 import { useQuery } from 'convex/react';
 import { api } from '@/convex/_generated/api';
 import { useLocalSearchParams, useRouter } from 'expo-router';
@@ -12,15 +12,50 @@ export default function PatientScreen() {
 
   return (
     <ThemedView style={styles.container}>
-      <ThemedText type="title">Patient Details</ThemedText>
-      <ThemedText>ID:</ThemedText>
-      <ThemedText>Name: </ThemedText>
-      <ThemedText>Triage Color: </ThemedText>
-
       {/* A button to navigate back to the previous screen */}
       <Button title="Go Back" onPress={() => router.back()} />
+    {/* TODO: insert actual logo */}
+      <SafeAreaView>
+        <Image 
+          source={require('@/assets/images/TriageNowLogo.png')} 
+          style={styles.logo}
+          resizeMode="contain"
+        />
+      </SafeAreaView>
+
+    {/* Patient ID
+    TODO: replace ID number placeholder */}
+      <SafeAreaView style={styles.ptID}>
+        <ThemedText type="title">Patient 10387</ThemedText>
+      </SafeAreaView>
+
+    {/* Care continuum Status */}
+      <SafeAreaView style={styles.careStatus}>
+        <ThemedText>Triage Status</ThemedText>
+      </SafeAreaView>
+
+    {/* Patient demographics section 
+    TODO: add in demographics info 
+    TODO: figure out how to input*/}
+      <SafeAreaView style={styles.patientDemographics}>
+      <ThemedText>Patient Demographics</ThemedText>
+        <ThemedText>ID:</ThemedText>
+        <ThemedText>Name: </ThemedText>
+        <ThemedText>Triage Color: </ThemedText>
+
+        
+      </SafeAreaView>
+
+    {/* patient care notes  
+    TODO: list notes added
+    TODO: allow edit/add notes */}
+      <SafeAreaView style={styles.patientCareNotes}>
+        <ThemedText>Patient Care Notes:</ThemedText>
+
+      </SafeAreaView>
     </ThemedView>
   );
+
 }
 
 const styles = StyleSheet.create({
@@ -28,6 +63,60 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 16,
     justifyContent: 'center',
-    alignItems: 'center',
+  },
+
+  logo: {
+    height: 50,
+    width: 100,
+    marginTop: 20,  
+  },
+
+  ptID: {
+    padding: 16,
+    marginTop: 0,
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    textAlign: 'left',
+  },
+
+  priority: {
+    padding: 16,
+    marginTop: 10,
+    borderRadius: 5,
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    backgroundColor: 'grey',
+  },
+
+
+  careStatus: {
+    padding: 16,
+    marginTop: 10,
+    borderRadius: 5,
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    backgroundColor: 'grey',
+  },
+
+  patientDemographics: {
+    flex: 1,
+    padding: 16,
+    margin: 3,
+    alignItems: 'flex-start',
+    backgroundColor: '#ededed',
+    borderRadius: 3,
+    borderWidth: 1, 
+    borderColor: 'grey',
+  },
+
+  patientCareNotes: {
+    flex: 1,
+    padding: 16,
+    margin: 3,
+    alignItems: 'flex-start',
+    backgroundColor: '#ededed',
+    borderRadius: 3,
+    borderWidth: 1, 
+    borderColor: 'grey',
   },
 });
