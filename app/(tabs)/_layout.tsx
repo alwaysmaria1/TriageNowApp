e098b206514ac9efd84bddb983ec51b4742d38c6
Micaela Rodriguez Steube <micaelarodriguezsteube@Micaelas-MacBook-Pro-2.local>
2025-03-11 15:31:14 -0400

    added currentUser state and setCurrentUser action to store.ts

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
+import React, {useState} from 'react';
 import { Platform } from 'react-native';
 
 import { HapticTab } from '@/components/HapticTab';
@@ -10,6 +10,8 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const [role, setRole] = useState(null); // Store user role
+
 
   return (
     <Tabs
--- components/lib/store.ts
+++ components/lib/store.ts
@@ -1,3 +1,4 @@
+import { current } from "immer";
 import { Patient, User } from "./types";
 import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
@@ -5,6 +6,7 @@ import { immer } from "zustand/middleware/immer";
 type State = {
     patients: Patient[];
     users: User[];
+    currentUser: User | null;
 }
 
 type Action = {
@@ -12,11 +14,14 @@ type Action = {
     setPatients: (patients: Patient[]) => void;
     addUser: (user: User) => void;
     setUsers: (users: User[]) => void;
+    setCurrentUser: (user: User) => void; // Function to update user
+
 }
 
 const initialState: State = {
     patients: [],
     users: [],
+    currentUser: null,
     // setPatients: () => {},
 }
 
@@ -36,7 +41,11 @@ export const useStore = create<State & Action>()(
             set({ users: [user, ...get().users] });
         },
 
-        setUsers: (users) => set({ users }),
-
+        setUsers: (users) => {set({ users })
+        },
+        
+        setCurrentUser: (user: User) => {
+            set({currentUser: user});
+        },
     }))
 );
